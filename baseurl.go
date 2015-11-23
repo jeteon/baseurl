95cd14774a88a271023c373c47d1a3bc4a781023
Neo <jeteon@users.noreply.github.com>
2015-11-23 16:15:10 +0200

    Grammatrical changes to comments in file

--- baseurl.go
+++ baseurl.go
@@ -5,13 +5,13 @@ import (
 	"strings"
 )
 
-// This func is returned base url from `net/http#Request`.
+// This function returns the base url from `net/http#Request`.
 //
-// If requested url is `http://localhost:8080/path/to/file`,
-// this func is returned `http://localhost:8080`.
+// If the request URL is `http://localhost:8080/path/to/file`,
+// this function returns `http://localhost:8080`.
 //
-// And If `net/http#Request` object is defined `TLS` property,
-// This func is returned `https://localhost:8080`.
+// If the `net/http#Request` object defines a `TLS` property,
+// this function returns `https://localhost:8080`.
 func BaseUrl(r *http.Request) string {
 	var proto string
 	if r.TLS != nil {
